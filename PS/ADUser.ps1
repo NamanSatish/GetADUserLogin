20a0633348e92a1bf39e25903fcc6f7632b7ba56
NamanSatish <naman.satish@gmail.com>
2019-11-27 13:56:42 -0800

    update

--- PS/ADUser.ps1
+++ PS/ADUser.ps1
@@ -1,4 +1,4 @@
-Get-ADUser -Filter *  -ResultPageSize 0 -Property SAMAccountName, LastLogonTimestamp |
-    Select-Object -Property SAMAccountName, @{ n = "LastLogonDate"; e = { [datetime]::FromFileTime( $_.lastLogonTimestamp ) } } |
+Get-ADUser -Filter *   -Property SAMAccountName, LastLogonDate |
+    Select-Object -Property SAMAccountName,LastLogonDate |
     Sort-Object -Property SAMAccountName, LastLogonDate |
     Export-CSV -NoTypeInformation "C:\dev\GetADUserLogin\myfile.csv"
